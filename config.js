10e7cf79668218d4a1ec272069a4da45d862b476
x1tedbtw <86682327+x1tedbtw@users.noreply.github.com>
2024-03-25 23:25:32 +0100

    Add files via upload

---
+++ config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  "title": "Test",
+  "name": "test",
+  "description": "for test",
+  "defaultLocale": "en",
+  "repository": 'https://github.com/x1tedbtw/Statusfy.git',
+  "locales": [
+    {
+      "code": "en",
+      "iso": "en-US",
+      "name": "English",
+      "file": "en.json"
+    }
+  ],
+  "content": {
+    "frontMatterFormat": "yaml"
+  }
+}
